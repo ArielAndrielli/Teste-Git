69b3c9b30d1aa639bb83eae6ed96da9597e999f1
Ariel Andrielli <arielandrielli100@gmail.com>
2022-01-06 09:57:44 -0300

    Adicionada uma mensagem escrito Opa eae

--- Exemplo Git/Program.cs
+++ Exemplo Git/Program.cs
@@ -7,6 +7,7 @@ namespace Exemplo_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Opa, eae!");
 
             Console.ReadLine();
         }
